0dccbf85400c67a3a2c93ed682861f782ff15ced
Dhananjay155 <dhananjaychoudhary017@gmail.com>
2024-11-08 20:57:03 +0530

    add sidepanel

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import doctorImg from "../../assets/images/doctor-img02.png";
 import starIcon from "../../assets/images/Star.png";
+import DoctorAbout from "./DoctorAbout";
+import Feedback from "./Feedback";
+import SidePanel from "./SidePanel";
 
 const DoctorDetails = () => {
   const [tab, setTab] = useState("about");
@@ -58,9 +61,17 @@ const DoctorDetails = () => {
                 Feedback
               </button>
             </div>
+
+            <div className="mt-[50px]">
+              {tab === "about" && <DoctorAbout />}
+              {tab === "feedback" && <Feedback />}
+            </div>
+            
           </div>
 
-          <div></div>
+          <div>
+            <SidePanel/>
+          </div>
         </div>
       </div>
     </section>
